1261335cebdb72d125187f95fe609f49b878326b
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-05-20 11:19:31 +0530

    first commit

---
+++ Social-Book-Network/src/test/java/com/ruwan/BookNetwork/SocialBookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruwan.BookNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
