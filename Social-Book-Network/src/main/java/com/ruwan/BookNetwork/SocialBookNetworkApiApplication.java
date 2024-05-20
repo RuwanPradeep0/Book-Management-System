d4b55804434772294edc3f09d42a8ae6b2a04243
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-05-20 19:48:55 +0530

    Rore Entity and Repositories

--- Social-Book-Network/src/main/java/com/ruwan/BookNetwork/SocialBookNetworkApiApplication.java
+++ Social-Book-Network/src/main/java/com/ruwan/BookNetwork/SocialBookNetworkApiApplication.java
@@ -2,7 +2,9 @@ package com.ruwan.BookNetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SocialBookNetworkApiApplication {
 
