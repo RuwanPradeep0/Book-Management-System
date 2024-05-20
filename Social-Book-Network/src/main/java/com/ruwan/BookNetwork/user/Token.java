e56da7bf16f6f4b8afa6ab0fae1c0f968fb85ec0
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-05-20 20:29:53 +0530

    Token entity and repository

---
+++ Social-Book-Network/src/main/java/com/ruwan/BookNetwork/user/Token.java
@@ -0,0 +1,33 @@
+package com.ruwan.BookNetwork.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token")
+@EntityListeners(AuditingEntityListener.class)
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId" , nullable = false)
+    private User user;
+
+
+}
